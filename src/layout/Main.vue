a0bee5549277cbca97fb40ca1f722c2b1f820a0e
tiancong <359193813@qq.com>
2025-08-08 09:32:26 +0800

    ci: 初始化

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,21 +1,17 @@
 <template>
-  <a-layout>
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+  <a-layout style="height: 100vh">
+    <a-layout-sider class="custom-sider" v-model:collapsed="collapsed" :trigger="null" collapsible>
       <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
-        </a-menu-item>
-      </a-menu>
+      <div class="menu-box">
+        <div class="menu-scroll">
+          <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+            <a-menu-item v-for="item in menuList" :key="item.key">
+              <component :is="item.icon" />
+              <span>{{ item.label }}</span>
+            </a-menu-item>
+          </a-menu>
+        </div>
+      </div>
     </a-layout-sider>
     <a-layout>
       <a-layout-header style="background: #fff; padding: 0">
@@ -26,11 +22,22 @@
         />
         <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
       </a-layout-header>
-      <a-layout-content
-        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
-      >
-        Content
-      </a-layout-content>
+      <TabGroup />
+      <div class="content-outer">
+        <div class="content-inner">
+          <a-layout-content
+            :style="{
+              margin: '12px 10px',
+              padding: '10px',
+              background: '#fff',
+              minHeight: 'calc(100vh - 64px - 24px - 20px)',
+            }"
+          >
+            Content
+            <div style="height: 900px"></div>
+          </a-layout-content>
+        </div>
+      </div>
     </a-layout>
   </a-layout>
 </template>
@@ -43,8 +50,56 @@ import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
 } from '@ant-design/icons-vue'
+import TabGroup from './TabGroup.vue'
 const selectedKeys = ref<string[]>(['1'])
 const collapsed = ref<boolean>(false)
+const menuList = ref([
+  {
+    key: '1',
+    icon: UserOutlined,
+    label: 'nav 1',
+  },
+  {
+    key: '2',
+    icon: VideoCameraOutlined,
+    label: 'nav 2',
+  },
+  {
+    key: '3',
+    icon: UploadOutlined,
+    label: 'nav 3',
+  },
+  {
+    key: '4',
+    icon: UserOutlined,
+    label: 'nav 4',
+  },
+  {
+    key: '5',
+    icon: VideoCameraOutlined,
+    label: 'nav 5',
+  },
+  {
+    key: '6',
+    icon: UploadOutlined,
+    label: 'nav 6',
+  },
+  {
+    key: '7',
+    icon: UserOutlined,
+    label: 'nav 7',
+  },
+  {
+    key: '8',
+    icon: VideoCameraOutlined,
+    label: 'nav 8',
+  },
+  {
+    key: '9',
+    icon: UploadOutlined,
+    label: 'nav 9',
+  },
+])
 </script>
 
 <script lang="ts">
@@ -73,5 +128,42 @@ export default {
 
 .site-layout .site-layout-background {
   background: #fff;
+}
+:deep(.ant-layout-sider-children) {
+  display: flex;
+  flex-direction: column;
+}
+
+.custom-sider {
+  display: flex;
+  flex-direction: column;
+  .menu-box {
+    flex: 1;
+    overflow: hidden;
+    .menu-scroll {
+      height: 100%;
+      overflow: auto;
+    }
+  }
+  .ant-menu {
+    overflow: auto;
+  }
+}
+.content-outer {
+  flex: 1;
+  overflow: hidden;
+  background-color: #f5f5f5;
+  .content-inner {
+    height: 100%;
+    overflow: auto;
+  }
+}
+
+::-webkit-scrollbar {
+  width: 4px;
+}
+::-webkit-scrollbar-thumb {
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
 }
 </style>
